9990cda1e5325590aa1d3d9f6f659f1c49be4f7d
LuukHSchoen <walono8@145.89.10.39>
2017-04-06 13:56:17 +0200

    rapid

--- src/model/Main.java
+++ src/model/Main.java
@@ -1,9 +1,12 @@
 package model;
 
+import model.persoon.Student;
+
 public class Main {
 	public static void main(String[] arg) {
+		Student s1 = new Student("Falco", "de", "Beer", "geheim", "falco.debeer@student.hu.nl", 1694547);
 		PrIS p1 = new PrIS();
-		System.out.println();
+		System.out.println(p1.getKlasVanStudent(s1));
 		
 		
 	}
