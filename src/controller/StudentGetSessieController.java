bee92c1c95a196bb15b842cc34da909820418911
Robin van Vlijmen <Robin van Vlijmen@DESKTOP-UJFMTNV.wifi.hu.nl>
2017-04-07 13:22:49 +0200

    Nieuwe controller voor eerste use case

---
+++ src/controller/StudentGetSessieController.java
@@ -0,0 +1,45 @@
+package controller;
+
+import java.util.Calendar;
+
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonObjectBuilder;
+
+import model.PrIS;
+import server.Conversation;
+import server.Handler;
+
+public class StudentGetSessieController implements Handler {
+	private PrIS informatieSysteem;
+	
+	public StudentGetSessieController(PrIS infoSys) {
+		informatieSysteem = infoSys;
+	}
+
+	public void handle(Conversation conversation) {
+	  if (conversation.getRequestedURI().startsWith("/my-absentCollegesKrijgen/collegeinfo")) {
+			collegeinfo(conversation);
+		}
+	}
+	
+  private void collegeinfo(Conversation conversation) {
+		JsonObject lJsonObjIn = (JsonObject) conversation.getRequestBodyAsJSON();
+  
+  	String lGebruikersnaam = lJsonObjIn.getString("username");	
+  	
+  	
+  	// Array gemistencolleges = gekregen.functie(datum, gebruikersnaam)
+
+		
+		JsonObjectBuilder lJsonObjectBuilder = Json.createObjectBuilder();
+		
+		lJsonObjectBuilder 
+		
+			.add("college", informatieSysteem.getStudent(lGebruikersnaam).getVolledigeAchternaam());
+
+		String lJsonOut = lJsonObjectBuilder.build().toString();
+		
+		conversation.sendJSONMessage(lJsonOut);										// terugsturen naar de Polymer-GUI!	}
+  }
+}
