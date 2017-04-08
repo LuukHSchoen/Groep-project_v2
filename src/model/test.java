e4b0db330f00e74f2c6d09789611e8f491f7c0e0
Robin van Vlijmen <Robin van Vlijmen@192.168.178.15>
2017-04-08 23:06:31 +0200

    Voor testing

---
+++ src/model/test.java
@@ -0,0 +1,16 @@
+package model;
+
+import java.util.ArrayList;
+
+import model.klas.Klas;
+import model.klas.Sessie;
+
+public class test {
+	public static void main(String[] arg) {
+		PrIS infoSysteem = new PrIS();
+		}
+		
+		
+	}
+
+
