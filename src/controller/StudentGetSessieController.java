7c1a1ff832f009afc2bca9e873426cc15a9f3ebe
Robin van Vlijmen <Robin van Vlijmen@192.168.178.15>
2017-04-08 14:00:28 +0200

    Aanpassingen

--- src/controller/StudentGetSessieController.java
+++ src/controller/StudentGetSessieController.java
@@ -1,12 +1,17 @@
 package controller;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 
 import javax.json.Json;
+import javax.json.JsonArrayBuilder;
 import javax.json.JsonObject;
 import javax.json.JsonObjectBuilder;
 
 import model.PrIS;
+import model.klas.Klas;
+import model.klas.Sessie;
+import model.persoon.Student;
 import server.Conversation;
 import server.Handler;
 
@@ -19,30 +24,38 @@ public class StudentGetSessieController implements Handler {
 
 	public void handle(Conversation conversation) {
 	  if (conversation.getRequestedURI().startsWith("/my-absent/sessiesinfo")) {
-			collegeinfo(conversation);
+			sessiesinfo(conversation);
 		}
 	}
 	
-  private void collegeinfo(Conversation conversation) {
+  private void sessiesinfo(Conversation conversation) {
 		JsonObject lJsonObjIn = (JsonObject) conversation.getRequestBodyAsJSON();
   
   	String lGebruikersnaam = lJsonObjIn.getString("username");
   	String datum = lJsonObjIn.getString("datum");
-  
   	
+  	Klas deklas = informatieSysteem.getKlasVanStudent(informatieSysteem.getStudent(lGebruikersnaam));
+  	ArrayList<Sessie> desessies = informatieSysteem.getSessiesOpDatumEnKlas(datum, deklas.getKlasCode());
   	
   	
   	// Array gemistencolleges = gekregen.functie(datum, gebruikersnaam)
 
 		
-		JsonObjectBuilder lJsonObjectBuilder = Json.createObjectBuilder();
-		
-		lJsonObjectBuilder 
-		
-			.add("test", informatieSysteem.getStudent(lGebruikersnaam).getVolledigeAchternaam());
+		JsonArrayBuilder lJsonArrayBuilder = Json.createArrayBuilder();	
 
-		String lJsonOut = lJsonObjectBuilder.build().toString();
+		for (Sessie deSessie : desessies) {									        // met daarin voor elke medestudent een JSON-object... 
+				String cursuscode = deSessie.getCursus().getcursusCode();
+				String begineneindtijd = deSessie.getCollege().getBeginEnEindTijd();
+				JsonObjectBuilder lJsonObjectBuildergeminstesessie = Json.createObjectBuilder(); // maak het JsonObject voor een student
+				lJsonObjectBuildergeminstesessie
+					.add("cursuscode", cursuscode)																//vul het JsonObject		     
+					.add("tijd", begineneindtijd);	
+			  lJsonArrayBuilder.add(lJsonObjectBuildergeminstesessie);													//voeg het JsonObject aan het array toe				     
+			}
+		String lJsonOutGemisteSessie = lJsonArrayBuilder.build().toString();												// maak er een string van
+		conversation.sendJSONMessage(lJsonOutGemisteSessie);					
+		}
 		
-		conversation.sendJSONMessage(lJsonOut);										// terugsturen naar de Polymer-GUI!	}
+										// terugsturen naar de Polymer-GUI!	}
   }
-}
+
