91d00caa55aa16267adc6e43780b4aaa5be1db13
LuukHSchoen <walono8@192.168.178.26>
2017-04-09 21:17:33 +0200

    StudentPresentieInzien perfectioneren

--- src/model/PrIS.java
+++ src/model/PrIS.java
@@ -113,13 +113,13 @@ public class PrIS {
 	public ArrayList<Presentie> studentPresentieInzien(String dat, Student deStud){
 		ArrayList<Presentie> studentPresenties = new ArrayList<Presentie>();
 		for(Sessie pSessie: deSessies){
-			if (pSessie.getCollege().getDatum() == dat){
-				Presentie p = new Presentie(deStud);
-				studentPresenties.add(p);		
+				for (Presentie depresentie : pSessie.getCollege().getdePresentie())	{
+					if (pSessie.getCollege().getDatum().equals(dat) && pSessie.getKlas().equals(getKlasVanStudent(deStud))){
+						studentPresenties.add(depresentie);
+				}
 			}
 		}
 		return studentPresenties;
-	
 	}
 	
 	public int aantalAbsenties(String dat, Student deStud){
